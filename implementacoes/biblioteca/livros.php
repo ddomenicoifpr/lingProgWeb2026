51eda3aa4fa22ee24e0a8921a1caf82826ce3620
Daniel Di Domenico <daniel.domenico@ifpr.edu.br>
2026-04-22 10:55:30 -0300

    Aula 22/04/2025

--- implementacoes/biblioteca/livros.php
+++ implementacoes/biblioteca/livros.php
@@ -1,3 +1,28 @@
+<?php
+
+//Exibir erros
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+require_once("util/Conexao.php");
+
+$conexao = Conexao::getConexao();
+//print_r($conexao);
+
+//Salvar o livro
+if(isset($_POST['titulo'])) {
+    echo "Já clicou no gravar!";
+}
+
+//Listagem dos livros
+$sql = "SELECT * FROM livros";
+$stm = $conexao->prepare($sql);
+$stm->execute();
+$livros = $stm->fetchAll();
+
+//echo "<pre>" . print_r($livros, true) . "</pre>";
+
+?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
@@ -11,7 +36,66 @@
 
     <h3>Listagem</h3>
 
+    <table border="1">
+        <!-- Cabeçalho -->
+        <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Gênero</th>
+            <th>Páginas</th>
+        </tr> 
+
+        <!-- Dados -->
+        <?php foreach($livros as $l): ?>
+            <tr>
+                <td><?= $l["id"] ?></td>
+                <td><?= $l["titulo"] ?></td>
+                <td>
+                    <?php
+                        if($l['genero'] == 'D')
+                            echo "Drama";
+                        else if($l['genero'] == 'F')
+                            echo "Ficção";
+                        else if($l['genero'] == 'R')
+                            echo "Romance";
+                        else if($l['genero'] == 'O')
+                            echo "Outro";                        
+                    ?>
+                </td>
+                <td><?= $l["qtd_paginas"] ?></td>
+            </tr>
+        
+        <?php endforeach; ?>
+    </table>
+
+
     <h3>Formulário</h3>
+
+    <form action="" method="POST">
+
+        <input type="text" placeholder="Informe o título"
+            name="titulo">
+
+        <br><br>
+
+        <select name="genero">
+            <option value="">---Selecione o gênero---</option>
+            <option value="D">Drama</option>
+            <option value="F">Ficção</option>
+            <option value="R">Romance</option>
+            <option value="O">Outro</option>
+        </select>
+
+        <br><br>
+
+        <input type="number" name="paginas" 
+            placeholder="Informe o número de páginas">
+
+        <br><br>
+
+        <button>Gravar</button>
+
+    </form>
     
 </body>
 </html>
--- implementacoes/biblioteca/util/Conexao.php
+++ implementacoes/biblioteca/util/Conexao.php
@@ -2,13 +2,26 @@
 
 class Conexao {
 
-    private PDO $conexao = null;
+    private static ?PDO $conexao = null;
 
-    public function getConexao(): PDO {
-        if($this->conexao == null) {
+    public static function getConexao(): PDO {
+        if(self::$conexao == null) {
             //Criar a conexao
+            $opcoes = array(
+                //Define o charset da conexão
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+                //Define o tipo do erro como exceção
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                //Define o tipo do retorno das consultas
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
+
+            self::$conexao = 
+                new PDO("mysql:host=mysql-server;dbname=db_biblioteca", 
+                        "root", 
+                        "root", 
+                        $opcoes);
         }  
 
-        return $this->conexao;
+        return self::$conexao;
     }
 }
