4271bf2f959e49da94ec377e7fc3ca345207c4d0
Daniel Di Domenico <daniel.domenico@ifpr.edu.br>
2026-06-17 10:19:31 -0300

    Aula 17/06/2026

---
+++ implementacoes/crud_alunos/view/alunos/listar.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(__DIR__ . "/../../util/Connection.php");
+
+
+$conn = Connection::getConnection();
+print_r($conn);
+
+?>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro de Alunos</title>
+</head>
+<body>
+    
+</body>
+</html>
